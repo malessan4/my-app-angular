042c5bbe718ab00717d3112e89cb67d35d8fe63d
Matias-Alessandrello <matialess@gmail.com>
2024-03-24 14:38:42 -0300

    hola mundo desde angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'App en Angular';
-  saludo = 'Saludos desde Angular';
+  title = 'Hola Mundo desde Angular';
 }
